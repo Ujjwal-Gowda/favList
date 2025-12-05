acb080b311051a3f00dbb88f54e178baaa6c7c28
ujjwal G <crystalninja0123@gmail.com>
2025-12-05 16:17:51 +0530

    path fixed

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
@@ -12,10 +14,12 @@ const favRoutes_1 = __importDefault(require("./routes/favRoutes"));
 const searchRoutes_1 = __importDefault(require("./routes/searchRoutes"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)({
-    origin: ["http://localhost:5173", "https://fav-list-chi.vercel.app/"],
+app.use(
+  (0, cors_1.default)({
+    origin: ["http://localhost:5173", "https://fav-list-chi.vercel.app"],
     credentials: true,
-}));
+  }),
+);
 app.use(express_1.default.json({ limit: "4mb" }));
 app.use(express_1.default.urlencoded({ limit: "4mb", extended: true }));
 app.use((0, cookie_parser_1.default)());
@@ -24,5 +28,5 @@ app.use("/auth", authroute_1.default);
 app.use("/search", searchRoutes_1.default);
 app.use("/favorites", favRoutes_1.default);
 app.listen(5000, () => {
-    console.log("server running on http://localhost:5000");
+  console.log("server running on http://localhost:5000");
 });
